3ad9e9af7addc702b4bcc9cd82bcb09b42ef35f1
Zaenal <jayker85@gmail.com>
2024-01-26 16:08:08 +0700

    first commit to craete repo

---
+++ bin/list.dart
@@ -0,0 +1,25 @@
+void main(){
+
+var names = <String>[];
+
+names.add('Zaenal');
+names.add('Anita');
+names.add('Siti');
+names.add('Zhafira');
+
+print(names);
+print(names[2]);
+
+names.add('Sultan');
+print(names);
+print(names.length);
+
+names.removeAt(2);
+print(names);
+print(names[2]);
+
+List<int> listAngka = [];
+listAngka = [2,3,4];
+print(listAngka);
+
+}
